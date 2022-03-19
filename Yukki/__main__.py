f9473c3a213bedda2a2bbac49c8b79bda61779de
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-19 13:12:09 +0800

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -94,7 +94,7 @@ async def initiate_bot():
             status="[bold blue]Importation Completed!",
         )
     console.print(
-        "[bold green]Congrats!! Yukki Music Bot has started successfully!\n"
+        "[bold green]Congrats!! Zebo Music Bot has started successfully!\n"
     )
     try:
         await app.send_message(
