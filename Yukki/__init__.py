fe52b795ea97e50aa86be816e0bcceb2a0f0682c
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-03-26 07:50:33 +0700

    Update __init__.py

--- Yukki/__init__.py
+++ Yukki/__init__.py
@@ -105,7 +105,7 @@ async def initiate_bot():
     )
     console.print(header)
     with console.status(
-        "[magenta] Zebo Music Bot Booting...",
+        "[magenta] Yukki Music Bot Booting...",
     ) as status:
         console.print("┌ [red]Booting Up The Clients...\n")
         await app.start()
